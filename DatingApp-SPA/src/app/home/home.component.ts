47c21f849f11b5dbabbe5934627dd88928b5573f
Einars Vilnis <vilniseinars@gmail.com>
2019-06-18 12:35:46 +0100

    update

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -24,14 +23,4 @@ export class HomeComponent implements OnInit {
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
   }
-
-  // getValues() {
-  //   this.http.get('http://localhost:5000/api/values')
-  //     .subscribe(response => {
-  //       this.values = response;
-  //     }, error => {
-  //       console.log('error accured', error);
-  //     });
-  // }
-
 }
